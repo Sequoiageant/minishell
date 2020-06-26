38afc5a2a74b55164da1d5e0b662f3d8bea6c670
Basile <basile.brunet@gmail.com>
2020-06-26 11:57:57 +0200

    brainstorming FSM dans parsing.h

--- parsing.h
+++ parsing.h
@@ -6,12 +6,72 @@
 /*   By: grim <grim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 09:59:10 by grim              #+#    #+#             */
-/*   Updated: 2020/06/24 15:36:12 by grim             ###   ########.fr       */
+/*   Updated: 2020/06/26 11:55:54 by grim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
+
+
+
+// etat normal
+
+// etat '
+// etat "
+// etat backslash
+
+// etat $
+
+// etat ;
+// etat |
+
+// echappement oui ou non (en fonction de la combinaison '', "" et backslash)
+// si non:
+//     etat ;
+//     etat |
+// si oui: etat normal
+
+// etat substitution oui ou non (en fonction de la combinaison '', "" et backslash)
+// si oui:
+//     etat $
+// si non:
+//     etat normal
+
+
+
+
+// buffer = une ou plusieurs pipelines
+// pipeline = une ou plusieurs commandes
+
+
+// cmd 1 ; cmd2 | cmd 3 | cmd4 ; cmd5
+// 3 pipeline avec 1 cmd, 3 cmd, 1 cmd
+
+
+// t_list  *pipeline_list
+
+// pipeline_list->content = (t_pipeline *)pipeline
+// pipeline_list->next = autre t_pipeline
+
+
+// typedef struct	s_pipeline
+// {
+//     t_list      *cmd;
+// }				t_pipeline;
+
+// cmd->content = (t_cmd*)cmd;
+
+// typedef struct s_cmd
+// {
+//     char		    **argv;
+// 	   int		    argc;
+//     int         output_file; // >
+//     int         output_file_append; // >>
+//     int         input_file; // <
+//     char        *file; // pour >, >> et <
+// }               t_cmd;
+
 
 
 typedef struct	s_cmd
