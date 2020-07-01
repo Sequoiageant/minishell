e56025a42dbb7f3241024443274f7a434345ddb6
Sequoiageant <j.nolleau88@gmail.com>
2020-07-01 16:11:35 +0200

    first ctrl-c implementation

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: grim <grim@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: julnolle <julnolle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 12:14:58 by grim              #+#    #+#             */
-/*   Updated: 2020/07/01 15:33:41 by grim             ###   ########.fr       */
+/*   Updated: 2020/07/01 16:09:47 by julnolle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,23 @@ int		ft_handle(char *buf, t_list **env)
 	return (0);
 }
 
+void	ctrlc_signal(int signum)
+{
+	int new_pid = 0; // prendre le pid du fork, en var globale ?
+	if (new_pid)
+	{
+		printf("pid = %d\n", new_pid);
+		kill(new_pid, signum);
+		// ft_putchar_fd('\n', 1);
+		// ft_putstr_fd("cmd: ", 1);
+	}
+	else
+	{
+		ft_putchar_fd('\n', 1);
+		ft_putstr_fd("new cmd: ", 1);
+	}
+}
+
 int		main()
 {
 	t_list	*env;
@@ -38,8 +55,13 @@ int		main()
 	// while (i < 1)
 	while (1)
 	{
+		if (signal(SIGINT, ctrlc_signal) == SIG_ERR)
+			return (1);
 		ft_putstr_fd("cmd: ", 1);
 		ret = read(1, buf, BUF_SIZE);
+		printf("ret = %d\n", ret);
+		if (!ret)
+			exit(0);
 		// printf("ret: %d\n", ret);
 		buf[ret - 1] = 0; // on a un \n qui s'ajoute à la fin du buffer, dont on ne veut pas
 		ft_handle(buf, &env);
@@ -47,4 +69,5 @@ int		main()
 	}
 	ft_lstclear(&env, &del_key_val);
 	free(buf);
+	return (0);
 }
