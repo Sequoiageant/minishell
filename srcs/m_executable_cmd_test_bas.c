1db6cd9184d035e4244859274410f24fcacbb17c
Basile <basile.brunet@gmail.com>
2020-07-20 15:34:59 +0200

    test bas: ok pour n pipe - y compris pas de pipe

--- srcs/m_executable_cmd_test_bas.c
+++ srcs/m_executable_cmd_test_bas.c
@@ -6,7 +6,7 @@
 /*   By: grim <grim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/02 18:51:03 by grim              #+#    #+#             */
-/*   Updated: 2020/07/20 15:21:30 by grim             ###   ########.fr       */
+/*   Updated: 2020/07/20 15:34:19 by grim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,28 +89,37 @@ int		ft_executable_cmd(t_list *cmd_list, t_list *env)
 	num_pipe = ft_build_pipes(cmd_list, &fd);
 	if ((g_glob.pid = fork()) == 0)
 	{
-		dup_close_pipes(fd, 0, fd[i][PIPE_WRITE], num_pipe);
+		if (cmd_list->next)
+			dup_close_pipes(fd, 0, fd[i][PIPE_WRITE], num_pipe);
 		ft_exec_cmd(cmd_list, env_tab);
 	}
 	cmd_list = cmd_list->next;
-	if ((g_glob.pid = fork()) == 0)
+	if (cmd_list)
 	{
-		dup_close_pipes(fd, fd[i][PIPE_READ], fd[i + 1][PIPE_WRITE], num_pipe);
-		ft_exec_cmd(cmd_list, env_tab);
+		while (cmd_list->next)
+		{
+			if ((g_glob.pid = fork()) == 0)
+			{
+				dup_close_pipes(fd, fd[i][PIPE_READ], fd[i + 1][PIPE_WRITE], num_pipe);
+				ft_exec_cmd(cmd_list, env_tab);
+			}
+			cmd_list = cmd_list->next;
+			i++;
+		}
+		if ((g_glob.pid = fork()) == 0)
+		{
+			dup_close_pipes(fd, fd[i][PIPE_READ], 0, num_pipe);
+			ft_exec_cmd(cmd_list, env_tab);
+		}
 	}
-	cmd_list = cmd_list->next;
-	i++;
-	if ((g_glob.pid = fork()) == 0)
+	i = 0;
+	while (i < num_pipe)
 	{
-		dup_close_pipes(fd, fd[i][PIPE_READ], 0, num_pipe);
-		ft_exec_cmd(cmd_list, env_tab);
+		close(fd[i][1]);
+		close(fd[i][0]);
+		wait(&status);
+		i++;
 	}
-	close(fd[0][1]);
-	close(fd[0][0]);
-	close(fd[1][0]);
-	close(fd[1][1]);
-	wait(&status); // doit attendre que la DERNIERE commande du pipe ait terminée // will wait for any child process -> il n'y en a juste 1 = premier fork (tous les autres fork sont faits à l'intérieur de ce child process)
-	wait(&status);
 	wait(&status);
 	if (WIFEXITED(status))
 		g_glob.ret = WEXITSTATUS(status);
