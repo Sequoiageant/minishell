abc001c1523bc28bfa289b26fb75aac68c2cfd25
Basile <basile.brunet@gmail.com>
2020-07-23 16:20:30 +0200

    gère plusieurs arguments de type -n

--- srcs/built_echo.c
+++ srcs/built_echo.c
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   built_echo.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julnolle <julnolle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: grim <grim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 12:15:34 by grim              #+#    #+#             */
-/*   Updated: 2020/07/22 17:37:04 by julnolle         ###   ########.fr       */
+/*   Updated: 2020/07/23 14:47:17 by grim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "mshell.h"
 
-void	ms_echo_suite(int argc, char *argv[], int i, int n_opt)
+void	ms_echo_suite(int argc, char *argv[], int num_opt)
 {
+	int i;
+
+	i = num_opt + 1;
 	while (i < argc - 1)
 	{
 		if (ft_strcmp(argv[i], "?") == 0)
@@ -29,30 +32,61 @@ void	ms_echo_suite(int argc, char *argv[], int i, int n_opt)
 		ft_putnbr_fd(g_glob.ret, 1);
 	else
 		ft_putstr_fd(argv[i], 1);
-	if (n_opt == FALSE)
+	if (num_opt == 0) // pas de "-n"
 		ft_putstr_fd("\n", 1);
+}
+
+
+int		is_option(char *str)
+{
+	int i;
+
+	if (str[0] != '-')
+		return(FALSE);
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] != 'n')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+int		count_options(char *argv[])
+{
+	int count;
+	int i;
+	
+	i = 1;
+	count = 0;
+	while (argv[i])
+	{
+		if (is_option(argv[i]))
+		{
+			count++;
+			i++;
+		}
+		else
+			return (count);
+	}
+	return (count);
 }
 
 int		ms_echo(int argc, char *argv[], t_list **env)
 {
-	int	i;
-	int	n_opt;
-
+	int	num_opt; // nombre d'arguments de type "-n"
+	
 	(void)env;
-	i = 1;
-	n_opt = FALSE;
-	if (argc == 1)
+	if (argc == 1) // pas de "-n" et pas d'argument
 	{
 		ft_putstr_fd("\n", 1);
 		return (SUCCESS);
 	}
-	if (ft_strncmp(argv[1], "-n", 2) == 0)
-	{
-		i = 2;
-		n_opt = TRUE;
-	}
-	if (n_opt && argc == 2)
+	num_opt = count_options(argv);
+	// printf("num opt: %d\n", num_opt);
+	if (num_opt && argc == num_opt + 1) // si une/plusieurs options de type "-n" et pas d'argument
 		return (SUCCESS);
-	ms_echo_suite(argc, argv, i, n_opt);
+	ms_echo_suite(argc, argv, num_opt);
 	return (SUCCESS);
 }
